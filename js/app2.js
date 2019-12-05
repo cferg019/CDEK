db2e454ab62795b14aeb6e480d2bff5ca4089983
Ermina_Joseph <ermina.joseph@gmail.com>
2019-12-04 22:14:19 -0500

    creating intial java to hide/show divs.  Also testing.  DO NOT APPROVE OR MERGE

---
+++ js/app2.js
@@ -0,0 +1,32 @@
+$(document).ready(function() { 
+    $("#when").hide();
+    $("#where").hide();
+    $("#submit-button").hide();
+    $("#next-button").hide();
+    $("#what").show();
+
+    $('select[name="dropdown"]').change(function(){
+        var activityChoice = $(this).val();
+        console.log(activityChoice);
+        $("#what").hide();
+        $("#where").show();
+        $("#next-button").show();
+    
+    });
+
+    $("#next-button").on("click", function() {
+        var placeChoice = $("#email").val().trim();
+        console.log(placeChoice);
+        $("#where").hide();
+        $("#next-button").hide();
+        $("#when").show();
+        $("#submit-button").show();
+    });
+
+    $("#submit-button").on("click", function() {
+        var dateChoice = $(".datepicker").datepicker({ dateFormat: "yy-mm-dd" });
+        console.log(dateChoice);
+    });
+
+
+});
