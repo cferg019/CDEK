b32585b5ffdecafb319b92c58c9565d193359368
Khine Wai <khine.wai@uconn.edu>
2019-12-07 09:39:52 -0500

    updates

--- app.js
+++ app.js
@@ -1,11 +1,50 @@
-var queryURL = "https://www.eventbriteapi.com/v3/users/me/?token=5PNZQK7RF6ZAWUJTMBTL"
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-
-    console.log(response);
 
 
+// $.ajax({
+//     method:"GET",
+//     url:"https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=lGxG3vAdLmUCh0Ip0y4Rx2KfHRHxfG5r",
+//     async:true,
+//     dataType: "json",
+//     success: function(json) {
+//                 console.log(json);
+//                 // Parse the response.
+//                 // Do other things.
+//              },
+//     error: function(xhr, status, err) {
+//                 // This time, we do not end up here!
+//              }
+//   });
 
+
+
+
+{/* <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> */}
+
+
+function openbrewerydb(state) {
+
+var brewryURL = "https://api.openbrewerydb.org/breweries?by_name=cooper&by_state=" + state; 
+    $.ajax({
+        url: brewryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+
+    var stateCity = $("").text(response.city);
+    var stateName = $("").text(response.name);
+    var stateAddress = $("").text(response.Stree + State + Postal_Code);
+    var 
+
+    });
 }
+
+function ticketmaster(event) {
+    var eventURL = "https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=lGxG3vAdLmUCh0Ip0y4Rx2KfHRHxfG5r"
+    $.ajax({
+        url: eventURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+    });
+}
+
